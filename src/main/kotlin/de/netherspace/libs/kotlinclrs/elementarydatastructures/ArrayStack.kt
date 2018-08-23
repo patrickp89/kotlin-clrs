7e1fa25fdc4990c0724bb8e848296f16af0f4d44
patrickp89 <patrickpreuss@netherspace.de>
2018-08-23 15:29:00 +0200

    The ArrayStack implementation now also SHRINKS the underlying array correctly

--- src/main/kotlin/de/netherspace/libs/kotlinclrs/elementarydatastructures/ArrayStack.kt
+++ src/main/kotlin/de/netherspace/libs/kotlinclrs/elementarydatastructures/ArrayStack.kt
@@ -4,18 +4,23 @@ import java.util.concurrent.atomic.AtomicInteger
 
 class ArrayStack<T> : Stack<T> {
 
-    private var a = Array<StackElement<T>>(10) {_ -> StackElement(null)}
+    private val initialArraySize = 10
+
+    private var a = Array<StackElement<T>>(initialArraySize) { _ -> StackElement(null)}
     private val top = AtomicInteger(0)
 
     override fun push(x: T): Boolean {
         if (top.get() == a.size - 1) {
-            resizeArray()
+            a = resizeArray(a)
         }
         a[top.incrementAndGet()] = StackElement(x)
         return true
     }
 
     override fun pop(): T? {
+        if (top.get() < a.size / 2) {
+            a = shrink(a)
+        }
         return if (!isEmpty()) {
             a[top.getAndDecrement()].element
         } else {
@@ -27,13 +32,34 @@ class ArrayStack<T> : Stack<T> {
         return top.get() == 0
     }
 
-    private fun resizeArray() {
-        val newSize = a.size * 2
-        var b = Array<StackElement<T>>(newSize) {_ -> StackElement(null)}
-        for (i in 0 until a.size) {
-            b[i] = a[i]
+    private fun resizeArray(A: Array<StackElement<T>>) : Array<StackElement<T>> {
+        return copy(A) { s: Int -> s * 2 }
+    }
+
+    private fun shrink(A: Array<StackElement<T>>) : Array<StackElement<T>> {
+        //check for min. threshold:
+        return if (A.size >= initialArraySize * 2 ) {
+            copy(A) { s: Int -> s / 2 }
+        } else {
+            A
         }
-        a = b
+    }
+
+    private fun copy(A: Array<StackElement<T>>, sizeOperator: (Int) -> Int): Array<StackElement<T>> {
+        val newSize = sizeOperator.invoke(A.size)
+        val b = Array<StackElement<T>>(newSize) {_ -> StackElement(null)}
+        for (i in 0 until minimum(A.size, newSize)) {
+            b[i] = A[i]
+        }
+        return b
+    }
+
+    private fun minimum(i: Int, j: Int): Int {
+        return if (i < j) {
+            i
+        } else {
+            j
+        }
     }
 
     class StackElement<T>(e: T?) {
